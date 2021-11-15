3e58b46a114648ec5e53ffb0fe1260913332914a
SomeJello <emarlette95@gmail.com>
2021-11-14 23:50:26 -0800

    Added dummy data for testing messages

--- MessageDrop.Web/Pages/Index.cshtml.cs
+++ MessageDrop.Web/Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace MessageDrop.Web.Pages
 {
@@ -7,14 +6,21 @@ namespace MessageDrop.Web.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        public List<String> messages;
+        public String message;
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            messages = new List<String>();
+            messages.Add("Cool messages");
+            messages.Add("wow way 2 go");
+            messages.Add("ur so cool");
         }
 
         public void OnGet()
         {
-
+            message = messages[messages.Count - 1];
         }
     }
 }
